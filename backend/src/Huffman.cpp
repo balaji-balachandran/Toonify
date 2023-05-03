ac5a25f8886444e80ee1182ab25b34eabe2ed4ab
David Opoku-Ware <deopokuware27@gmail.com>
2023-05-03 18:01:48 -0500

    Decode should work now

--- backend/src/Huffman.cpp
+++ backend/src/Huffman.cpp
@@ -2,22 +2,27 @@
 using namespace std;
 vector<int> decode(string sequence, string binary_val_map) {
     vector<string> comma_sep;
-    vector<int> result;
-    std::map<string, string> binary_grp;
-    stringstream s_stream(sequence);
+    map<string, string> binary_grp;
+    stringstream s_stream(binary_val_map);
     while (s_stream.good()) {
         string substr;
         getline(s_stream, substr, ',');
-        result.push_back(substr);
+        comma_sep.push_back(substr);
     }
-    for (int i = 0; i < result.size(); i++) {
-        int pos = result.at(i).find(':');
-        binary_grp.at(result.at(i).substr(0, pos)) = result.at(i).substr(pos, result[i].size() - pos);
+    for (auto elem : comma_sep) {
+        vector<string> row_content;
+        stringstream s_stream(elem);
+        while (s_stream.good()) {
+            string substr;
+            getline(s_stream, substr, ':');
+            row_content.push_back(substr);
+        }
+        binary_grp.insert(pair<string, string>(row_content[0], row_content[1]));
     }
     string bt_;
     for (auto x : sequence) {
         bt_ += x;
-        if (binary_grp.contains(bt_)) {
+        if (binary_grp.count(bt_)) {
             result.push_back(stoi(binary_grp.at(bt_)));
             bt_ = "";
         }
--- backend/src/Huffman.h
+++ backend/src/Huffman.h
@@ -3,5 +3,6 @@
 #include <vector>
 #include <sstream>
 #include <map>
+#include <utility>
 
 std::vector<int> decode(std::string sequence, std::string binary_val_map);
