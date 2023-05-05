1a808eac8c2325b01bd75c3c13c7e9892d4c6a9b
David Opoku-Ware <deopokuware27@gmail.com>
2023-05-05 00:40:35 -0500

    Matching decode declare/call

--- backend/src/Huffman.h
+++ backend/src/Huffman.h
@@ -5,4 +5,4 @@
 #include <map>
 #include <utility>
 
-std::vector<int> decode(std::string sequence, std::string binary_val_map);
+std::vector<unsigned char> decode(std::string sequence, std::string binary_val_map);
